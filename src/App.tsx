6874e73267d01dca9ed4dce5e00e2c27f1d8a244
szachMati <donek8@gmail.com>
2021-01-19 00:44:29 +0100

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import ButtonAppBar from "./app/components/AppBar";
 import SignIn from "./app/components/SignIn";
 
-
 function App() {
-    const [show, setShown] = useState<boolean>(false);
+  const [show, setShown] = useState<boolean>(false);
 
-    const handleClick = () => {
-        setShown(true);
-    }
+  const handleClick = () => {
+    setShown(true);
+  };
+
   return (
-      <React.Fragment>
-        <ButtonAppBar onClick={handleClick} />
-          {show && <SignIn />}
-      </React.Fragment>
+    <React.Fragment>
+      <ButtonAppBar onClick={handleClick} />
+      {show && <SignIn />}
+    </React.Fragment>
   );
 }
 
--- src/app/components/AppBar.tsx
+++ src/app/components/AppBar.tsx
@@ -1,50 +1,57 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
 }));
 
 interface ButtonAppBarProps {
-    onClick: () => void;
+  onClick: () => void;
 }
 
-function ButtonAppBar(props: ButtonAppBarProps)  {
-    const classes = useStyles();
+function ButtonAppBar(props: ButtonAppBarProps) {
+  const classes = useStyles();
 
-    const handleClick = () => {
-        props.onClick();
-    }
+  const handleClick = () => {
+    props.onClick();
+  };
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Weather prediction
-                    </Typography>
-                    <Button onClick={() => handleClick()} color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Weather prediction
+          </Typography>
+          <Button onClick={() => handleClick()} color="inherit">
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default ButtonAppBar;
