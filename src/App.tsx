1c75f61d356cc1d721cf1311a9c2f9864098e33d
szachMati <donek8@gmail.com>
2021-01-18 23:59:53 +0100

    c.d

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from "@material-ui/core";
+import ButtonAppBar from "./app/components/AppBar";
+
+const useStyles = makeStyles((theme) => ({
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p>test commit</p>
-    </div>
+    <ButtonAppBar />
   );
 }
 
