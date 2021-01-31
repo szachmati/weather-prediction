62422c12a5c5d80e215604d5cd5d5946f9d9dc3f
szachMati <donek8@gmail.com>
2021-01-31 14:00:14 +0100

    keep logged user

--- src/app/store/app.store.tsx
+++ src/app/store/app.store.tsx
@@ -1,9 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers} from "@reduxjs/toolkit";
 import { notificationReducer, userReducer } from "./app.store.reducer";
+import thunk from "redux-thunk";
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist'
+
+const persistConfig = {
+  key: 'root',
+  blackList: ['notification'], // doesnt keep notificationReducer in Local Storage
+  storage
+}
+
+export const persistedReducer = combineReducers({
+  notification: notificationReducer,
+  user: persistReducer(persistConfig, userReducer)
+});
 
 export const store = configureStore({
-  reducer: {
-    notification: notificationReducer,
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk]
 });
+
+export const persistedStore = persistStore(store);
